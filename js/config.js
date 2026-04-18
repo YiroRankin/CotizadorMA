6c0fbaa5fdc568598f804ae58f3e6f690b3b93d5
YiroRankin <amable@misionadmision.com>
2026-04-18 15:28:54 -0600

    Add runtime config file

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window.COTIZADOR_CONFIG = {
+  dataUrls: {
+    pricing: "./data/pricing.json",
+    specialists: "./data/specialists.json",
+    courses: "./data/courses.json",
+  },
+  quoteLogging: {
+    enabled: false,
+    endpointUrl: "",
+  },
+};
