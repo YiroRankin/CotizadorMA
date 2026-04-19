a2aec3b973435e2f1ea65fd27e36279a4519b21a
YiroRankin <amable@misionadmision.com>
2026-04-18 19:04:47 -0600

    Update config.js

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
 window.ADMIN_APP_CONFIG = {
-  historyEndpointUrl: "",
+  historyEndpointUrl: "https://script.google.com/macros/s/AKfycbxbkT9cFWdPXIhzpDTPcVbtdjKqmF-30HiXeSw2VeTZL9CyKqQ6UQiJR246pp66854/exec",
   title: "Panel Comercial · Cotizador MA",
 };
